4dcd923a3113e3ecb8201b4e69c18083575eab76
Hi <Hi@LAPTOP-HD74BTM2>
2022-08-13 14:44:53 +0700

    ádasdasd Add project files.

---
+++ WebsiteBanGiay/Models/HomeModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebsiteBanGiay.Context;
+namespace WebsiteBanGiay.Models
+{
+    public class HomeModel
+    {
+        public List<SanPham> ListProduct { get; set; }
+        public List<Category> ListCategory { get; set; }
+    }
+}
